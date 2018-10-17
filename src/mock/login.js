16bdefeda50c884487fc2fef48a9d5213881fcae
arfurs <arfurs@163.com>
2018-10-17 13:30:34 +0800

    Add Account

--- src/mock/login.js
+++ src/mock/login.js
@@ -1,25 +1,26 @@
 import { param2Obj } from '@/utils'
 
 const userMap = {
-  admin: {
+  NicoLi: {
     roles: ['admin'],
-    token: 'admin',
-    introduction: '我是超级管理员',
+    token: 'NicoLi',
+    introduction: '超级管理员',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
     name: 'Super Admin'
   },
-  editor: {
-    roles: ['editor'],
-    token: 'editor',
-    introduction: '我是编辑',
+  TingTingGao: {
+    roles: ['admin'],
+    token: 'TingTingGao',
+    introduction: '超级管理员',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Normal Editor'
+    name: 'Super Admin'
   }
 }
 
 export default {
   loginByUsername: config => {
     const { username } = JSON.parse(config.body)
+    console.log(userMap[username])
     return userMap[username]
   },
   getUserInfo: config => {
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,7 +3,7 @@
  */
 
 export function isvalidUsername(str) {
-  const valid_map = ['admin', 'editor']
+  const valid_map = ['NicoLi', 'TingTingGao']
   return valid_map.indexOf(str.trim()) >= 0
 }
 
