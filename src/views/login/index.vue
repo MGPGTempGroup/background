0ceca83abdfa9dfff60352a41f006e8402a293e5
arfurs <arfurs@163.com>
2018-10-20 15:03:11 +0800

    login tip update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -40,11 +40,11 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">{{ $t('login.logIn') }}</el-button>
 
       <div class="tips">
-        <span>{{ $t('login.username') }} : admin</span>
+        <span>{{ $t('login.username') }} : NicoLi</span>
         <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
       </div>
       <div class="tips">
-        <span style="margin-right:18px;">{{ $t('login.username') }} : editor</span>
+        <span style="margin-right:18px;">{{ $t('login.username') }} : TingtingGao</span>
         <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
       </div>
 
