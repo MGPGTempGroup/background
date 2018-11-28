a79f75e5f8f5720767fb205fb2d469e5fc7137bb
arfurs <arfurs@163.com>
2018-11-29 01:51:06 +0800

    utils update

--- src/utils/parseData2ConditionalParams.js
+++ src/utils/parseData2ConditionalParams.js
@@ -8,7 +8,7 @@ const arrayIsEmpty = arr => !(arr instanceof Array) || arr.length === 0
 /**
  * 这段代码为后端条件查询接口所编写：将表单对象数据转换为条件查询字符串（url参数）
  */
-export default function parseData2ConditionalParams({ fuzzy = {}, dateRange = {}, contains = {}}) {
+export default function parseData2ConditionalParams({ fuzzy = {}, dateRange = {}, contains = {}, between = {}, basics = {}}) {
   const paramsArr = []
   // 转换模糊查询参数
   paramsArr.push(
@@ -33,6 +33,31 @@ export default function parseData2ConditionalParams({ fuzzy = {}, dateRange = {}
         return 'contains_' + item[0] + '[]=' + val
       }).join('&')
     }).join('&')
+  )
+  // 转换between查询参数（between查询）
+  paramsArr.push(
+    Object.entries(between)
+      .filter(item => !arrayIsEmpty(item[1]) &&
+      item[1].length === 2 &&
+      item[1].every(Number))
+      .map(item => {
+        return item[1].map(val => {
+          return item[0] + '_between[]=' + val
+        }).join('&')
+      }).join('&')
+  )
+
+  // 基础操作符查询参数（>、>=、<=、<>...）
+  paramsArr.push(
+    Object.entries(basics)
+      .map(conditionalArr => {
+        const [operator, conditionals] = conditionalArr
+        return Object.entries(conditionals).map(item => {
+          const [column, value] = item
+          if (!value) return undefined
+          return column + '_' + operator + '=' + value
+        }).filter(Boolean).join('&')
+      }).filter(Boolean).join('&')
   )
 
   return paramsArr.filter(Boolean).join('&')
