8e7bf655908c3968f8db44bed7d45240020d4645
arfurs <arfurs@163.com>
2019-01-14 21:34:33 +0800

    业务组件提取

---
+++ src/businessComponent/CustomerIdentitySelect/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="customer-identity-select" >
+    <el-select
+      v-model="values"
+      :placeholder="$t('selectByIdentityPlaceholder')"
+      multiple
+      filterable>
+      <el-option
+        v-for="(item, index) in availableIdentities"
+        :key="index"
+        :label="$t(`customerIdentity.${item.label}`)"
+        :value="item.value" />
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomerIdentitySelect',
+  model: {
+    prop: 'identities',
+    event: 'change'
+  },
+  props: {
+    identities: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      availableIdentities: [
+        { label: 'landlord', value: 1 },
+        { label: 'vendor', value: 4 },
+        { label: 'other', value: 5 },
+        { label: 'tenant', value: 2 },
+        { label: 'purchaser', value: 3 }
+      ]
+    }
+  },
+  computed: {
+    values: {
+      get() {
+        return this.identities
+      },
+      set(identities) {
+        this.$emit('change', identities)
+      }
+    }
+  },
+  watch: {
+    values: {
+      handler(identities) {
+        this.$emit('change', identities)
+      }
+    }
+  }
+}
+</script>
